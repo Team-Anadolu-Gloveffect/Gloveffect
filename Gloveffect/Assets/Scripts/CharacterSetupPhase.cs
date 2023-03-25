2cb0f96c91420579fcd1a00d863782fbf49a15d2
Fernley <fernley1952@gmail.com>
2023-03-25 18:44:48 +0300

    Added Object Pooling System
    I added object pooling system with 2 different prefabs.

---
+++ Gloveffect/Assets/Scripts/CharacterSetupPhase.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CharacterSetupPhase : MonoBehaviour
+{
+    [SerializeField] private Collider collider;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private float movementSpeed = 2f;
+    [SerializeField] private float turnSpeed = 2f;
+    //[SerializeField] private float bulletSpeed = 10;
+    //[SerializeField] private GameObject fireBulletPrefab, waterBulletPrefab;
+    //[SerializeField] private Transform gunMuzzle;
+    [SerializeField] private bool isPoolable;
+
+    //[SerializeField] private FirePoolingManager poolingManager;
+
+    private Vector2 _movementInputValues, _rotationInputValues;
+
+    public static bool IsFire, IsWater;
+
+    private void Awake()
+    {
+        AssignComponents();       
+    }
+
+    private void AssignComponents()
+    {
+        if (collider == null) collider = GetComponent<Collider>();
+        if (characterController == null) characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _movementInputValues = GetMovementInputData();
+        _rotationInputValues = GetRotationMovementData();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!Input.anyKey) return;
+        Move();
+        Rotate();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("FireGun"))
+            IsFire= true;
+        if (other.CompareTag("WaterGun"))
+            IsWater = true;
+
+    }
+
+
+    private Vector2 GetMovementInputData()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private Vector2 GetRotationMovementData()
+    {
+        return new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+    }
+
+    #region Movement
+
+    private void Move()
+    {
+        Vector3 move = new Vector3(_movementInputValues.x * movementSpeed * Time.fixedDeltaTime, 0,
+            _movementInputValues.y * movementSpeed * Time.fixedDeltaTime);
+
+        move = (Camera.main.transform.forward * move.z) + (Camera.main.transform.right * move.x);
+        move.y = 0;
+
+        characterController.Move(new Vector3(move.x, -1,
+            move.z));
+    }
+
+    private void Rotate()
+    {
+        float targetAngle = Mathf.Atan2(_rotationInputValues.x, _rotationInputValues.y) * Mathf.Rad2Deg +
+                            Camera.main.transform.eulerAngles.y;
+
+        Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * turnSpeed);
+    }
+
+    #endregion
+
+    //private void FireBulletWithPooling()
+    //{
+    //    var bullet = FirePoolingManager.Instance.DequeuePoolableGameObject();
+    //    bullet.transform.position = gunMuzzle.position;
+    //    bullet.GetComponent<BulletController>().IsCalledByPooling = true;
+    //    bullet.GetComponent<Rigidbody>().AddForce(gunMuzzle.forward * bulletSpeed, ForceMode.Impulse);
+    //}
+}
---
+++ Gloveffect/Assets/Scripts/FireGun.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour
+{
+    [SerializeField] private FirePoolingManager poolingManager;
+    [SerializeField] private Transform gunMuzzle;
+    [SerializeField] private float bulletSpeed = 10;
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireBulletWithPooling();
+        }
+    }
+    private void FireBulletWithPooling()
+    {
+        var bullet = FirePoolingManager.Instance.DequeuePoolableGameObject();
+        bullet.transform.position = gunMuzzle.position;
+        bullet.GetComponent<BulletController>().IsCalledByPooling = true;
+        bullet.GetComponent<Rigidbody>().AddForce(gunMuzzle.forward * bulletSpeed, ForceMode.Impulse);
+    }
+}
