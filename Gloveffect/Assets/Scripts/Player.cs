a088e861874e2002d3b28eef40b7d63b3c031227
Mert Eren <metrorn@outlook.com>
2023-03-26 15:58:21 +0300

    WallRide Added

--- Gloveffect/Assets/Scripts/Player.cs
+++ Gloveffect/Assets/Scripts/Player.cs
@@ -18,7 +18,6 @@ public class Player : MonoBehaviour
     public Transform ground;
     public LayerMask groundLayer;
     public float groundDistance = 0.1f;
-    //public int jumpsPerformed = 0;
     public int jumpsPerformed = 0;
     public int maxJumps = 2;
 
@@ -26,6 +25,14 @@ public class Player : MonoBehaviour
     public Transform cameraFollower;
     public float mouseSensitivity = 700f;
     private float cameraVerticalRotation;
+
+    //WallRunning
+    public LayerMask wallMask;
+    public float wallRunForce, maxWallRunTime, maxWallSpeed;
+    bool isWallRight, isWallLeft;
+    bool isWallRunning;
+    public float maxWallRunCameraTilt, wallRunCameraTilt;
+    public Transform orientation;
 
     void Start()
     {
@@ -38,6 +45,8 @@ public class Player : MonoBehaviour
         PlayerMovement();
         CameraMovement();
         Jump();
+        CheckForWall();
+        WallRunInput();
     }
 
     void PlayerMovement()
@@ -82,6 +91,73 @@ public class Player : MonoBehaviour
 
         //prevents the conflict of velocities between jump and movement
         characterController.Move(velocity);
+
+        //WallRunning
+        /**
+        if (isWallRunning)
+        {
+            canJump = false;
+
+            //normal jump
+            if (isWallLeft && !Input.GetKey(KeyCode.D) || isWallRight && !Input.GetKey(KeyCode.A))
+            {
+                characterController.Move(Vector2.up * jumpHeight * 1.5f);
+                characterController.Move(Vector3.up * jumpHeight * 0.5f);
+            }
+
+            //sidwards wallhop
+            if (isWallRight || isWallLeft && Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) characterController.Move(-orientation.up * jumpHeight * 1f);
+            if (isWallRight && Input.GetKey(KeyCode.A)) characterController.Move(-orientation.right * jumpHeight * 3.2f);
+            if (isWallLeft && Input.GetKey(KeyCode.D)) characterController.Move(orientation.right * jumpHeight * 3.2f);
+
+            //Always add forward force
+            characterController.Move(orientation.forward * jumpHeight * 1f);
+
+            velocity = Vector3.zero;
+        }
+        **/
+    }
+
+    void WallRunInput()
+    {
+        if(Input.GetKey(KeyCode.D) && isWallRight) StartWallRun();
+        if(Input.GetKey(KeyCode.A) && isWallLeft) StartWallRun();
+    }
+
+    void StartWallRun()
+    {
+        gravityModifier = 10f;
+        isWallRunning = true;
+
+        if(velocity.magnitude <= maxWallSpeed)
+        {
+            characterController.Move(orientation.forward * wallRunForce * Time.deltaTime);
+
+            if(isWallRight)
+            {
+                characterController.Move(orientation.right * wallRunForce / 5 * Time.deltaTime);
+            }
+            else
+            {
+                characterController.Move(-orientation.right * wallRunForce / 5 * Time.deltaTime);
+            }
+        }
+    }
+
+    void StopWallRun()
+    {
+        gravityModifier = 10f;
+        isWallRunning = false;
+    }
+
+    void CheckForWall()
+    {
+        isWallRight = Physics.Raycast(transform.position, orientation.right, 1f, wallMask);
+        isWallLeft = Physics.Raycast(transform.position, -orientation.right, 1f, wallMask);
+
+        if(!isWallLeft && !isWallRight) StopWallRun();
+
+        if (isWallLeft || isWallRight) jumpsPerformed = 0;
     }
 
     void CameraMovement()
@@ -94,5 +170,20 @@ public class Player : MonoBehaviour
 
         transform.Rotate(Vector3.up * mouseX);
         cameraFollower.localRotation = Quaternion.Euler(cameraVerticalRotation, 0f, 0f);
+
+        //fix
+
+        cameraFollower.transform.localRotation = Quaternion.Euler(cameraVerticalRotation, 0, wallRunCameraTilt);
+        orientation.transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+        if (Mathf.Abs(wallRunCameraTilt) < maxWallRunCameraTilt && isWallRunning && isWallRight)
+            wallRunCameraTilt += Time.deltaTime * maxWallRunCameraTilt * 2;
+        if (Mathf.Abs(wallRunCameraTilt) < maxWallRunCameraTilt && isWallRunning && isWallLeft)
+            wallRunCameraTilt -= Time.deltaTime * maxWallRunCameraTilt * 2;
+
+        if (wallRunCameraTilt > 0 && !isWallRight && !isWallLeft)
+            wallRunCameraTilt -= Time.deltaTime * maxWallRunCameraTilt * 2;
+        if (wallRunCameraTilt < 0 && !isWallRight && !isWallLeft)
+            wallRunCameraTilt += Time.deltaTime * maxWallRunCameraTilt * 2;
     }
 }
