c8fcd686bf4fc70de11ec48e1613a772545af904
Fernley <fernley1952@gmail.com>
2023-04-03 11:17:12 +0300

    Melee Attack Finalized.
    Melee attack finalized with basic script and temporal animation.

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float meleeAttackDamage = 10f;
+    [SerializeField] private float attackRange = 1f;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            MeleeAttack();
+        }
+    }
+
+    private void MeleeAttack()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit) && hit.distance <= attackRange)
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+                Debug.Log("There is an enemy");
+                hit.collider.GetComponent<Enemy>().TakeDamage(meleeAttackDamage);
+                animator.SetTrigger("MeleeAttack");
+            }
+        }
+    }
+}
